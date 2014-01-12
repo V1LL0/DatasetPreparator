9df5bbe27d03c5c05a8744909d1142b404b3a3e2
massimo <v.cestarelli@gmail.com>
2014-01-12 15:36:53 +0100

    dal file review ora vengono generati due file:

--- src/it/sii/dataset/DatasetPreparator.java
+++ src/it/sii/dataset/DatasetPreparator.java
@@ -16,25 +16,48 @@ public class DatasetPreparator {
 		
 		Utilities utilities = new Utilities();
 		
-		String fileDSExtension = fileDatasetName.split("\\.")[0];
+		String fileDS_NoExtension = fileDatasetName.split("\\.")[0];
 		
 		File fileDataset = utilities.newFileIstance(fileDatasetName);
 		
-		File fileResult = utilities.newFileIstance(fileDSExtension+"_training.json");
-		File fileTestResult = utilities.newFileIstance(fileDSExtension+"_test.json");
+		File fileResult = utilities.newFileIstance(fileDS_NoExtension+"_training.json");
+		File fileTestResult = utilities.newFileIstance(fileDS_NoExtension+"_test.json");
+		File fileResultsResult = utilities.newFileIstance(fileDS_NoExtension+"_result.json");
+		
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fileDataset));
 			FileWriter writer = new FileWriter(fileResult);
 			FileWriter writerTest = new FileWriter(fileTestResult);
+			FileWriter writerResults = new FileWriter(fileResultsResult);
+			
 			
 			while(reader.ready()){
 				String line = reader.readLine();
 				if(utilities.percentageGuidedBoolean(percentage))
-					writer.write(line);		
-				else
-					writerTest.write(line);
+					writer.write(line+"\n");		
+				else{
+					utilities.writeLineTestAndResult(line, writerTest, writerResults);
+				}
 			}
+			
+	/**
+	 * Prove di stampa:		
+	 */
+			/**
+			BufferedReader readerFinal = new BufferedReader(new FileReader(fileDS_NoExtension+"_test.json"));
+			BufferedReader readerFinalResults = new BufferedReader(new FileReader(fileDS_NoExtension+"_result.json"));
+			System.out.println("TEST:");
+			for(int i=0; i<20; i++){
+				System.out.println(readerFinal.readLine());
+			}
+			System.out.println("\n\n\nRESULTS: ");
+			for(int i=0; i<20; i++){
+				System.out.println(readerFinalResults.readLine());
+			}
+			**/
+
+			
 		
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -54,15 +77,36 @@ public class DatasetPreparator {
 		DatasetPreparator preparator = new DatasetPreparator();
 		double percentage = 0.7;
 		
-		String fileDatasetUserName="full_dataset/user.json";
-		String fileDatasetBusinessName="full_dataset/business.json";
+//		String fileDatasetUserName="full_dataset/user.json";
+//		String fileDatasetBusinessName="full_dataset/business.json";
 		String fileDatasetReviewName="full_dataset/review.json";
 		
-		preparator.createPercentageOfDataset(fileDatasetUserName, percentage);
-		preparator.createPercentageOfDataset(fileDatasetBusinessName, percentage);
+//		preparator.createPercentageOfDataset(fileDatasetUserName, percentage);
+//		preparator.createPercentageOfDataset(fileDatasetBusinessName, percentage);
 		preparator.createPercentageOfDataset(fileDatasetReviewName, percentage);
 		
-		
+	
+		/***
+		 * Prove di stampa:		
+		 */
+		/**
+		BufferedReader readerFinal;
+		try {
+			readerFinal = new BufferedReader(new FileReader(fileDatasetReviewName));
+			
+			for(int i=0; i<20; i++){
+				System.out.println(readerFinal.readLine());
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		**/
+	
 		
 	}
 
--- src/it/sii/dataset/utilities/Utilities.java
+++ src/it/sii/dataset/utilities/Utilities.java
@@ -1,6 +1,8 @@
 package it.sii.dataset.utilities;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Utilities {
 
@@ -19,12 +21,12 @@ public class Utilities {
 		if (System.getProperty("os.name").toLowerCase().contains("windows"))
 			return false;			
 		return true;
-		}
+	}
 
+	
 	/**
- * Crea un nuova istanza di file, occupandosi di capire il sistema operativo sottostante (Unix o Windows)
- * @return
- */
+	 * Crea un nuova istanza di file, occupandosi di capire il sistema operativo sottostante (Unix o Windows)
+	 */
 	
 	public File newFileIstance(String abstractPath){
 		final String completeTrainingBackupFilePath;
@@ -35,6 +37,27 @@ public class Utilities {
 		return new File(completeTrainingBackupFilePath);
 	}
 
+	
+	
+	public void writeLineTestAndResult(String line, FileWriter writerTest, FileWriter writerResults){
+		
+		String[] jsonParts = line.split(",");
+		String stars = jsonParts[5].split(": ")[1];
+		String businessID = jsonParts[jsonParts.length-1].split(": ")[1].split("}")[0];
+		String userID = jsonParts[3].split(": ")[1];
+		
+		
+		try {
+			writerTest.write(userID+"\t"+businessID+"\n");
+			writerResults.write(stars+"\n");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 }
